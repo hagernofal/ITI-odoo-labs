beefebeb254a3f508956aee19569ef1eb7de3591
hagernofal <hagernofal8@gmail.com>
2026-06-13 13:13:35 +0300

    feat: complete hms odoo lab 1 , create my first model and view

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "ITI_Zagzig",
+    'author': 'ITI',
+    'website': '',
+    'version': '19.0.0',
+    'description': 'This is my first System',
+    'data': [
+        'views/hms_patient_views.xml'
+    ]
+}
